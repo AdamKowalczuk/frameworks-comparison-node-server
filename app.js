b8ca74074cc5614c509b7dc71d5f234ee6879a65
Adam <adam.kowalczuk@poczta.fm>
2023-09-04 21:21:44 +0200

    Create basic project structure

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const routes = require("./routes/userRoutes");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger");
+
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+
+// Allow Cross-Origin requests
+app.use(cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Connect the database
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("DB connection Successfully!");
+  });
+
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("App is working"));
+
+app.use("/api", routes);
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+module.exports = {
+  app,
+};
